a5241bb4e61541c324d7f8af198169c2cac3a026
Siva Nutakki <siva9963365904@gmail.com>
2026-06-02 20:59:56 +0530

    Setting Up FastAGI in GoLang

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func HandleAGIRequest(conn net.Conn) {
+	// Placeholder for handling AGI requests
+	// You would read from the connection, parse the AGI commands, and respond accordingly
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error reading line:", err)
+			break
+		}
+		if strings.TrimSpace(line) == "" {
+			break
+		}
+		fmt.Println("AGI ENV:", line)
+	}
+
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":5473")
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Printf("FastAGI server running on port 5473\n")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Error accepting connection:", err)
+		}
+		go HandleAGIRequest(conn)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Siva_Nutakki/DialBRX-FastAGI
+
+go 1.25.5
